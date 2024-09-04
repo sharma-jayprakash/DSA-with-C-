459205f4de163cbc97eb036598de7b9df94c6cbc
sharma-jayprakash <178124387+sharma-jayprakash@users.noreply.github.com>
2024-09-04 16:02:48 +0530

    Add files via upload
    hi there, 
    Documenting my dsa journey one Github its day 1 and I learn syntax of c++.

---
+++ first.cpp
@@ -0,0 +1,42 @@
+// write a program to print hello world 
+
+// #include <iostream>
+// using namespace std; 
+// int main()
+// {
+//     cout<<"hello world"<<endl <<"how are you "<<endl ;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     variable decleared by 4 byte 
+//     int a =20 ,b= 20 ;
+//     cout<< a+b;
+// char d = 'a';
+// cout<< d;
+// float f = 3.1344;
+// cout<<f;
+// double g  = 4.048775749;
+// cout<<g ;
+
+// bool b = true ;
+// cout<<b ;
+// }
+
+
+
+
+#include<iostream>
+using namespace std; 
+int main ()
+{
+    int a, b; 
+    cout<<"enter the number a :"<<endl ;
+    cout <<"enter the number b "<<endl ;  
+    cin >>a>>b; 
+    cout <<a+b ; 
+}
+
